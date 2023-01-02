91455d69bb616e26bd22705b122a419a699d2f14
sodamjeong <sodam828@naver.com>
2023-01-02 16:20:22 +0900

    Add TASK

---
+++ task/JAN01_1.py
@@ -0,0 +1,88 @@
+# 문제 1
+number = int(input("숫자를 입력해주세요. > ")) # 10
+print(number + 10) 
+
+# 문제 2
+food = input("좋아하는 음식을 입력해주세요.") # 피자
+print(food)
+
+# 문제 3
+name = input("이름을 입력해주세요.") # 정소담
+year = int(input("태어난 년도를 입력해주세요.")) # 1992
+age = int(2023) - year + 1
+
+print("저의 이름은",name,"이고, 올해 나이는",age,"세 입니다.")
+print("저의 이름은",name,"이고, 올해 나이는",int(2023)- year + 1 ,"세 입니다.")
+
+# 문제 4
+word1 = input("첫 번째 문장을 입력해주세요.") # hello
+word2 = input("두 번째 문장을 입력해주세요.") # world
+
+print(word1+word2)
+
+# 문제 5
+number1 = int(input("숫자를 입력해주세요.")) # 10
+
+print(-number1)
+
+# 문제 6
+number1 = int(input("첫 번째 숫자를 입력해주세요.")) # 10
+number2 = int(input("두 번째 숫자를 입력해주세요.")) # 3
+
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 // number2)
+print(number1 % number2)
+
+# 문제 7
+number1 = int(input("첫 번째 숫자를 입력해주세요.")) # 1
+number2 = int(input("두 번째 숫자를 입력해주세요.")) # 2
+number3 = int(input("세 번째 숫자를 입력해주세요.")) # 3
+
+print((number1 + number2 + number3) // 3 )
+
+# 문제 8
+number1 = int(input("첫 번째 숫자를 입력해주세요.")) # 2
+number2 = int(input("두 번째 숫자를 입력해주세요.")) # 54
+number3 = int(input("세 번째 숫자를 입력해주세요.")) # 1
+number4 = int(input("네 번째 숫자를 입력해주세요.")) # 6
+number5 = int(input("다섯 번째 숫자를 입력해주세요.")) # 3
+
+print([number1, number2, number3, number4, number5])
+
+# 문제 9
+word1 = input("문자열을 입력해주세요.") # hello
+number1 = int(input("숫자를 입력해주세요.")) # 3
+
+print(word1 * number1)
+
+# 문제 10
+
+# 1.
+number1 = int(input("첫 번째 숫자를 입력해주세요.")) # 1
+print(number1)
+number2 = int(input("두 번째 숫자를 입력해주세요.")) # 2
+print(number1 + number2)
+number3 = int(input("세 번째 숫자를 입력해주세요.")) # 3
+print(number1 + number2 + number3)
+number4 = int(input("네 번째 숫자를 입력해주세요.")) # 4
+print(number1 + number2 + number3 + number4)
+number5 = int(input("다섯 번째 숫자를 입력해주세요.")) # 5
+print(number1 + number2 + number3 + number4 + number5)
+
+# 2.
+number1 = int(input("첫 번째 숫자를 입력해주세요.")) # 1
+print(number1)
+number2 = int(input("두 번째 숫자를 입력해주세요.")) # 2
+a = (number1 + number2)
+print(a)
+number3 = int(input("세 번째 숫자를 입력해주세요.")) # 3
+b = (a + number3)
+print(b)
+number4 = int(input("네 번째 숫자를 입력해주세요.")) # 4
+c = (b + number4)
+print(c)
+number5 = int(input("다섯 번째 숫자를 입력해주세요.")) # 5
+d = (c + number5)
+print(d)
