7e28b4e2552a636ad1b439988af95bdf5e0bc58e
sodamjeong <sodam828@naver.com>
2023-01-04 14:05:14 +0900

    Add JAN04 for task

---
+++ practice/practice.py
@@ -0,0 +1,25 @@
+# n = input()
+
+# for idx in range(len(n)):
+#     print(n[idx])
+
+# for i in range(6):
+#     if i % 2  == 0:
+#      print(i)
+
+# n = int(input())
+# a = 0
+# b = 0
+# while a < n + 1:
+#     b += a
+#     a += 1
+# print(b)
+
+
+n = int(input())
+m = 0
+for i in range(1,n+1):
+    m += i
+    
+print(m)
+
