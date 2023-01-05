567be891a38271a2696079ba88eca39002cac8db
sodamjeong <sodam828@naver.com>
2023-01-06 00:20:57 +0900

    Add practice05

--- practice/practice05.py
+++ practice/practice05.py
@@ -6,6 +6,31 @@
 
 # print(random.sample(range(1,46), 6))
 
+# import datetime
+
+# print(datetime.datetime.now())
+
+# grades = {'john' : 80, 'eric' : 90}
+# print(grades.keys())
+# print(grades.values())
+# print(grades.items())
+
+# for name, score in grades.items():
+#     print(name,score)
+
+# n = [1, 5, 6, 6, 12, 5, 123, 5, 4]
+
+# import random
+
+# print(random.randint(4,15))
+# print(random.choice(n))
+# random.shuffle(n)
+# print(n)
+# print(random.sample(n, 2))
+
 import datetime
 
+print(datetime.date(2023, 1, 5))
+print(datetime.date.today())
+print(datetime.datetime.today())
 print(datetime.datetime.now())
