cbf0ff2760602a6e31aa0ea5acf7a851492e1bd7
sodamjeong <sodam828@naver.com>
2023-01-07 23:12:57 +0900

    practice

--- practice/practice.py
+++ practice/practice.py
@@ -68,9 +68,110 @@
 
 # print(a, b, c, d, e, f)
 
-A, B, C = map(int,input().split())
+# A, B, C = map(int,input().split())
 
-print((A + B) % C)
-print(((A % C) + (B % C)) % C)
-print((A * B) % C)
-print(((A % C) * (B % C)) % C)
+# print((A + B) % C)
+# print(((A % C) + (B % C)) % C)
+# print((A * B) % C)
+# print(((A % C) * (B % C)) % C)
+
+# A, B = map(int,input().split())
+# C = int(input())
+# D = B + C
+# H = int(((A * 60) + D) / 60)
+# M = ((A * 60) + D) % 60
+
+# if H >= 24:
+#     H -= 24
+
+# print(H, M)
+
+# A, B, C = map(int,input().split())
+
+# if A == B == C:
+#     print((A * 1000) + 10000)
+# elif (A == B) or (A == C):
+#     print((A * 100) + 1000)
+# elif (B == C):
+#     print((B * 100) + 1000)
+# else:
+#     print(max(A,B,C) * 100)
+
+# X = int(input())
+# N = int(input())
+# d = 0
+
+# for i in range(N):
+#     a,b = map(int,input().split())
+#     d += (a * b)
+# if d == X:
+#     print('YES')
+# else:
+#     print('NO')
+
+# import sys
+
+# T = int(input())
+
+
+# for i in range(T):
+#     a,b = map(int,sys.stdin.readline().split())
+#     print(a+b)
+
+# T = int(input())
+
+# for i in range(1,T + 1):
+#     a,b = map(int,input().split())
+#     print(f'Case #{i}:',(f'{a} + {b} ='),(a+b))
+
+# N = int(input())
+
+# for i in range(N):
+#     print(' ' * (N - i - 1) + '*' * (i+1))
+
+
+# import sys
+
+# while True:
+#     try:
+#         a,b = map(int,sys.stdin.readline().split())
+#         print(a + b)
+#     except:
+#         break
+
+# X = int(input())
+# Y = X
+# n = 0
+
+# while True:
+#     a = Y // 10
+#     b = Y % 10
+#     c = (a + b) % 10
+#     Y = b * 10 + c
+#     n += 1
+#     if Y == X:
+#         print(n)
+#         break
+
+# N, X = map(int,input().split())
+# A = list(map(int,input().split()))
+# n = {}
+
+# for i in A:
+#     if i < X:
+#         n[i] = 1
+
+# for key,value in n.items():
+#     print(key,end = " ")
+
+
+N, X = map(int,input().split())
+A = list(map(int,input().split()))
+n = []
+
+for i in A:
+    if i < X:
+        n.append(i)
+
+for a in n:
+    print(a,end=" ")
