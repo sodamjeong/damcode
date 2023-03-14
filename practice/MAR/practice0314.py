d64f38fe47ba0171a42b9aefc591f5938f66d124
sodamjeong <sodam828@naver.com>
2023-03-14 23:34:03 +0900

    Created practice0314

---
+++ practice/MAR/practice0314.py
@@ -0,0 +1,18 @@
+# 15650 N과M(2)
+
+n,m = list(map(int,input().split()))
+num = []
+
+def arr(i):
+    if len(num) == m:
+        print(*num)
+        return
+    
+    for j in range(i,n+1):
+        if j not in num:
+            num.append(j)
+            arr(j+1)
+            num.pop()
+arr(1)
+
+
